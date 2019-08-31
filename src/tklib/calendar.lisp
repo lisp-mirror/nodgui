1ba408b8059782c17ed1fc8c086276c5b75e1ac1
cage <cage-invalid@invalid>
2019-08-31 09:39:01 +0200

    - added docstrings.

--- src/tklib/calendar.lisp
+++ src/tklib/calendar.lisp
@@ -1,12 +1,32 @@
+;; This software is Copyright (c) 2003-2010  Peter Herth <herth@peter-herth.de>
+;; Portions Copyright (c) 2005-2010 Thomas F. Burdick
+;; Portions Copyright (c) 2006-2010 Cadence Design Systems
+;; Portions Copyright (c) 2010 Daniel Herring
+;; Portions Copyright (c) 2019 cage
+
+;; The  authors  grant you  the  rights  to  distribute and  use  this
+;; software as  governed by the  terms of  the Lisp Lesser  GNU Public
+;; License  (http://opensource.franz.com/preamble.html), known  as the
+;; LLGPL.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT  ANY  WARRANTY;  without   even  the  implied  warranty  of
+;; MERCHANTABILITY or FITNESS  FOR A PARTICULAR PURPOSE.   See the GNU
+;; General Public License for more details.
+
 (in-package :nodgui.tklib.calendar)
 
 (named-readtables:in-readtable nodgui.tcl-emitter:nodgui-force-escape-syntax)
 
 (define-constant +calendar-library-name+ "widget::calendar" :test #'string=)
 
-(define-constant +calendar-date-format-m/d/y+ "%m/%d/%Y" :test #'string=)
+(define-constant +calendar-date-format-m/d/y+ "%m/%d/%Y"
+  :test          #'string=
+  :documentation "Format of this date as day (2 digits) / month (2 digits) / year (4 digits)")
 
-(define-constant +calendar-date-format-d/m/y+ "%d/%m/%Y" :test #'string=)
+(define-constant +calendar-date-format-d/m/y+ "%d/%m/%Y"
+  :test #'string=
+  :documentation "Format of this date as month (2 digits) / day (2 digits) / year (4 digits)")
 
 (define-constant +calendar-supported-language+
     '(:de :en :es
@@ -48,6 +68,27 @@
                         (highlight-color (cl-colors:as-rgb "FFCC00"))
                         (shade-color     (cl-colors:as-rgb "888888"))
                         (font            +tk-default-font+))
+  "Make a calendar widget.
+Parameters are:
+
+- language, the language the calendar is displayed; legal values are the ones in
+'+calendar-supported-language+' (default :en);
+
+- date-format, the format of the inner date of this widget; legal values are:
+'+calendar-date-format-d/m/y+' and '+calendar-date-format-d/m/y+'
+(default '+calendar-date-format-d/m/y+');
+
+- first-day, the day of the week shown as the first column of the calendar: legal values are
+:monday and :sunday (default :monday);
+
+- highlight-color, background color of the selected day (should be a cl-color:rgb struct);
+  default color #xFFCC00;
+
+- shade-color, color of the shaded elements shown (should be a cl-color:rgb struct);
+  default color #x888888;
+
+- font, font used by the calendar default: '+tk-default-font+'.
+"
   (assert (member language +calendar-supported-language+))
   (assert (or (string= date-format +calendar-date-format-d/m/y+)
               (string= date-format +calendar-date-format-m/d/y+)))
@@ -64,6 +105,12 @@
                    :font           font)))
 
 (defmethod (setf command) (val (object calendar))
+  "Set the function to be called when a day is selected (by clicking),
+the function should accepts a  parameter: the string corresponding the
+selected   date  in   a   format  specified   when   the  widget   was
+instanced (see: make-calendar).
+
+That date can be parsed by parse-selected-date"
   (nodgui::add-callback (nodgui::name object) val)
   (format-wish "~a configure -command {callbackstring ~a}"
                (widget-path object)
@@ -71,6 +118,7 @@
   val)
 
 (defmethod (setf text) :after (val (object calendar))
+  "See: set-date or set-date*"
   (format-wish "~A configure -textvariable {text_~a}"
                (widget-path object)
                (nodgui::name object))
@@ -81,9 +129,11 @@
 (defgeneric set-date* (object date))
 
 (defun cget-date-format (calendar)
+  "Convenience function to get the string corresponding to the internal date of widget"
   (cget calendar :dateformat))
 
 (defmethod set-date ((object calendar) (day integer) (month integer) (year integer))
+  "Set the date for this calendar (that also will be shown and selected by the widget)"
   (let* ((date-format    (cget-date-format object))
          (format-control "~a/~a/~a")
          (date-as-string (cond
@@ -94,11 +144,17 @@
     (setf (text object) date-as-string)))
 
 (defmethod set-date* ((object calendar) (date string))
+    "Set the date for this calendar (that also will be shown and selected by the widget).
+The second parameter should be a string matching format of: +calendar-date-format-m/d/y+ or
++calendar-date-format-d/m/y+"
   (multiple-value-bind (day month year)
       (parse-selected-date object date)
     (set-date object day month year)))
 
 (defun parse-selected-date (widget date-as-text)
+  "Parse a string according to the internal date format of widget,
+(see: (setf command)). The allowed formats are: +calendar-date-format-d/m/y+ and
++calendar-date-format-m/d/y+"
   (let ((parsed      (mapcar #'parse-integer (split "/" date-as-text)))
         (date-format (cget-date-format widget)))
     (cond
--- src/tklib/package.lisp
+++ src/tklib/package.lisp
@@ -1,3 +1,19 @@
+;; This software is Copyright (c) 2003-2010  Peter Herth <herth@peter-herth.de>
+;; Portions Copyright (c) 2005-2010 Thomas F. Burdick
+;; Portions Copyright (c) 2006-2010 Cadence Design Systems
+;; Portions Copyright (c) 2010 Daniel Herring
+;; Portions Copyright (c) 2019 cage
+
+;; The  authors  grant you  the  rights  to  distribute and  use  this
+;; software as  governed by the  terms of  the Lisp Lesser  GNU Public
+;; License  (http://opensource.franz.com/preamble.html), known  as the
+;; LLGPL.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT  ANY  WARRANTY;  without   even  the  implied  warranty  of
+;; MERCHANTABILITY or FITNESS  FOR A PARTICULAR PURPOSE.   See the GNU
+;; General Public License for more details.
+
 (defpackage :nodgui.tklib.calendar
   (:use :cl
         :alexandria
@@ -13,4 +29,6 @@
    :make-calendar
    :set-date
    :set-date*
-   :parse-selected-date))
+   :parse-selected-date)
+  (:documentation "Wrapper for Calendar in tklib (https://core.tcl-lang.org/tklib/home).
+Tklib *must* be installed on a system for this wrapper to works" ))
